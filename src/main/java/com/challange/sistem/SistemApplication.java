127799f79a199f8e396fb6575b3b3ebc4a5a4bc5
Weverthon Medeiros <weverthon.medeiros@mastercard.com>
2020-02-28 19:52:42 +0000

    removed unused import

--- src/main/java/com/challange/sistem/SistemApplication.java
+++ src/main/java/com/challange/sistem/SistemApplication.java
@@ -1,6 +1,5 @@
 package com.challange.sistem;
 
-import org.apache.commons.codec.binary.Hex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
