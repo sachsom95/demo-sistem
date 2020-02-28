3688ce214f768f36f7e576774321c03d5206cec4
Weverthon Medeiros <weverthon.medeiros@mastercard.com>
2020-02-28 19:50:58 +0000

    First commit

---
+++ src/main/java/com/challange/sistem/SistemApplication.java
@@ -0,0 +1,14 @@
+package com.challange.sistem;
+
+import org.apache.commons.codec.binary.Hex;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/challange/sistem/SistemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challange.sistem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
